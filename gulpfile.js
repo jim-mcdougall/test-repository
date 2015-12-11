983ba9803175b6c41d0ce02e62e81259fbfbaed3
Jim McDougall <jim.mcdougall@manchester.ac.uk>
2015-12-11 13:30:25 +0000

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,17 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/*.coffee']
+var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
-]
+];
 
-var sassSources = ['components/sass/style.scss']
+var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 
 gulp.task('coffee', function() {
@@ -47,7 +49,9 @@ gulp.task('compass', function(){
 gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
-	gulp.watch('components/sass/*.scss', ['compass'])	
+	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function(){
@@ -55,7 +59,17 @@ gulp.task('connect', function(){
 		root: 'builds/development/',
 		livereload: true
 	})
-	
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())	
+});
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+	.pipe(connect.reload())	
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
